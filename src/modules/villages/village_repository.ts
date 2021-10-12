a980a6ec0f6ad7b297ca16cf9480c857486cf634
ayocodingit <ayocodingit@gmail.com>
2021-10-12 14:07:37 +0700

    add knex postgis for mapping geo json

--- src/modules/villages/village_repository.ts
+++ src/modules/villages/village_repository.ts
@@ -1,12 +1,19 @@
 import database from '../../config/database';
 import { Village as Entity } from './village_entity';
+import knexPostgis from 'knex-postgis';
+const st = knexPostgis(database);
 
 export namespace Village {
   export const Villages = () => database<Entity.Struct>('villages')
 
   export const findAllWithLocation = () => {
     const query = Villages()
-      .select('id', 'name', 'level', 'location')
+      .select(
+        'id',
+        'name',
+        'level',
+        st.asGeoJSON('location')
+      )
       .where('is_active', true)
       .orderBy('name', 'asc')
 
