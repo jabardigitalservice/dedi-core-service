6919a984b267511cf49d91659962090085b455d9
ayocodingit <ayocodingit@gmail.com>
2021-10-14 12:46:57 +0700

    simplify code paginate

--- src/helpers/paginate.ts
+++ src/helpers/paginate.ts
@@ -2,14 +2,11 @@ export const perPage = (requestQuery: {
   per_page: string,
   current_page: string
 }) => {
-  const perPageNumber = Number(requestQuery.per_page)
-  const currentPageNumber = Number(requestQuery.current_page)
-
-  const perPage = requestQuery.per_page && !isNaN(perPageNumber) ? perPageNumber : 20
-  const currentPage = requestQuery.current_page && !isNaN(currentPageNumber) ? currentPageNumber : 1
+  const perPageNumber = Number(requestQuery.per_page) || 20
+  const currentPageNumber = Number(requestQuery.current_page) || 1
 
   return {
-    perPage,
-    currentPage
+    perPage: perPageNumber,
+    currentPage: currentPageNumber
   }
 }
