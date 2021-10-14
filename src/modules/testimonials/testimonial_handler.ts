958bbebf99306d16c4385fba25a1823a0dab49d0
hafizulf <hafizulf68@gmail.com>
2021-10-14 14:28:12 +0700

    refCode: standardize testimonial handler response

--- src/modules/testimonials/testimonial_handler.ts
+++ src/modules/testimonials/testimonial_handler.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express'
 import httpStatus from 'http-status'
+import { Testimonial as Entity } from './testimonial_entity'
 import { Testimonial as Service } from './testimonial_service'
 
 const router = express.Router()
@@ -7,7 +8,9 @@ const router = express.Router()
 router.get(
   '/v1/testimonials',
   (req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.OK).json(Service.findAll())
+    const result: Entity.ResponseFindAll = Service.findAll()
+
+    res.status(httpStatus.OK).json(result)
   }
 )
 
