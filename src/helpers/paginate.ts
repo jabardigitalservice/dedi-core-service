e785107b2c60016adf48acacc660220c2b2e8eae
ayocodingit <ayocodingit@gmail.com>
2021-10-14 12:48:34 +0700

    simplify code paginate

--- src/helpers/paginate.ts
+++ src/helpers/paginate.ts
@@ -2,11 +2,8 @@ export const perPage = (requestQuery: {
   per_page: string,
   current_page: string
 }) => {
-  const perPageNumber = Number(requestQuery.per_page) || 20
-  const currentPageNumber = Number(requestQuery.current_page) || 1
+  const perPage = Number(requestQuery.per_page) || 20
+  const currentPage = Number(requestQuery.current_page) || 1
 
-  return {
-    perPage: perPageNumber,
-    currentPage: currentPageNumber
-  }
+  return { perPage, currentPage }
 }
