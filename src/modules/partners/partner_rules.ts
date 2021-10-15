cb581f8acda136cf70a9b9ae43285a244652cbe8
ayocodingit <ayocodingit@gmail.com>
2021-10-15 09:35:31 +0700

    update ci add run build

--- src/modules/partners/partner_rules.ts
+++ src/modules/partners/partner_rules.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export namespace Partner {
   export const findAll = Joi.object({
     name: Joi.string().allow(null),
-    perPage: Joi.number().allow(null),
-    page: Joi.number().allow(null)
+    per_page: Joi.number().allow(null),
+    current_page: Joi.number().allow(null)
   })
 }
