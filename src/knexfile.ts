9a60326f2060c7facabe14a1830c064e216c471a
ayocodingit <ayocodingit@gmail.com>
2021-10-15 13:50:52 +0700

    remove default test

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -31,13 +31,13 @@ export default {
   development: databaseConfig,
   production: databaseConfig,
   test: {
-    client: config.get('db.connection.test', 'mysql'),
+    client: config.get('db.connection.test'),
     connection: {
-      host: config.get('db.host.test', '127.0.0.1'),
-      port: config.get('db.port.test', 3306),
-      user: config.get('db.user.test', 'root'),
-      password: config.get('db.password.test', ''),
-      database: config.get('db.database.test', 'dedi_test')
+      host: config.get('db.host.test'),
+      port: config.get('db.port.test'),
+      user: config.get('db.user.test'),
+      password: config.get('db.password.test'),
+      database: config.get('db.database.test')
     },
     ...locationDatabase
   }
