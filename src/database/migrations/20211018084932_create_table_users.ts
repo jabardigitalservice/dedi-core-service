cf7241bbe39928aa686721241bbcaa0d7de8829b
hafizulf <hafizulf68@gmail.com>
2021-10-18 08:55:57 +0700

    init table users

---
+++ src/database/migrations/20211018084932_create_table_users.ts
@@ -0,0 +1,19 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.hasTable('users').then(function (exists) {
+    if (!exists) {
+      return knex.schema.createTable('users', function (table) {
+        table.uuid('id').primary()
+        table.string('name', 100).notNullable()
+        table.text('description').notNullable()
+        table.string('avatar').notNullable()
+        table.string('type', 15).notNullable()
+      })
+    }
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('users')
+}
