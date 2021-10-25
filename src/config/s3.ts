5dec21bc6665d6d3e25ae7dc29f1d97bbbd6222a
ayocodingit <ayocodingit@gmail.com>
2021-10-25 11:44:01 +0700

    lowercase variable aws

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -1,7 +1,7 @@
-import AWS from 'aws-sdk';
+import aws from 'aws-sdk';
 import config from '.';
 
-export default new AWS.S3({
+export default new aws.S3({
   accessKeyId: config.get('aws.access.key.id'),
   secretAccessKey: config.get('aws.secret.access.key'),
   region: config.get('aws.default.region')
