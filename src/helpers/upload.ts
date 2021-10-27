2ec7fa626af5086f1d3c273aad57d0b6347ff176
ayocodingit <41193120+ayocodingit@users.noreply.github.com>
2021-10-27 14:47:49 +0700

    update helper upload (#30)
    * rename multer to helper upload, add error max file error message
    
    * fileTypes default png|jpg|jpeg
    
    * add gitignore tmp
    
    * update type: string, manipulate message error
    
    * remove log and fix lint
    
    * use if else
    
    * remove example upload s3

---
+++ src/helpers/upload.ts
@@ -0,0 +1,96 @@
+import path from 'path'
+import lang from '../lang'
+import { Express, Request, RequestHandler, Response } from 'express'
+import multer, { FileFilterCallback } from 'multer'
+import { HttpError } from '../handler/exception'
+import httpStatus from 'http-status'
+import storage from '../config/storage'
+import { ParamsDictionary } from 'express-serve-static-core'
+
+interface StructErrors {
+  [key: string]: string[]
+}
+
+interface RequestFile {
+  req: Request
+  res: Response
+  fieldName: string
+  allowFileType?: string
+  fileSize?: number
+  isRequired?: boolean
+}
+
+interface UploadPromise {
+  upload: RequestHandler<ParamsDictionary, any, any, any, Record<string, any>>
+  requestFile: RequestFile
+}
+
+const formatError = (fieldName: string, message: string): HttpError => {
+  const errors: StructErrors = {
+    [fieldName]: [message]
+  }
+
+  return new HttpError(httpStatus.UNPROCESSABLE_ENTITY, JSON.stringify(errors), true)
+}
+
+const checkFileType = (file: Express.Multer.File, cb: FileFilterCallback, type: string = 'png|jpg|jpeg') => {
+  const fileTypes = new RegExp(type)
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = fileTypes.test(file.mimetype)
+
+  if (mimetype && extname) return cb(null, true)
+
+  const customMessage = {
+    attribute: file.fieldname,
+    values: type.split('|').join(', ')
+  }
+
+  cb(formatError(file.fieldname, lang.__('error.file.mimetypes', customMessage)))
+}
+
+const getError = (err: any, requestFile: RequestFile): HttpError => {
+  const error: HttpError = formatError(requestFile.fieldName, null)
+
+  const customMessage = {
+    attribute: requestFile.fieldName,
+  }
+
+  if (err && err.code === 'LIMIT_FILE_SIZE') {
+    error.message = lang.__('error.file.size', customMessage)
+  } else if (err && typeof err.code !== 'string') {
+    error.message = err.message
+  } else if (!err && requestFile.isRequired && requestFile.req.file === undefined) {
+    error.message = lang.__('error.file.doesntExist', customMessage)
+  }
+
+  return error
+}
+
+const uploadPromise = (file: UploadPromise): Promise<Express.Multer.File | null> => {
+  return new Promise((resolve, reject) => {
+    file.upload(file.requestFile.req, file.requestFile.res, (err: any) => {
+      const error = getError(err, file.requestFile)
+      const isError = JSON.parse(error.message)
+      if (isError[file.requestFile.fieldName][0]) reject(error)
+      resolve(file.requestFile.req.file || null)
+    })
+  })
+}
+
+export const uploadLocalSingle = (requestFile: RequestFile): Promise<Express.Multer.File | null> => {
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: requestFile.fileSize
+    },
+    fileFilter: (req: Request, file, cb: FileFilterCallback) => {
+      checkFileType(file, cb, requestFile.allowFileType)
+    }
+  })
+    .single(requestFile.fieldName)
+
+  return uploadPromise({
+    upload,
+    requestFile
+  })
+}
