2cc6da249920f91e263e31b94990e4f42a82af04
ayocodingit <ayocodingit@gmail.com>
2021-11-02 13:31:28 +0700

    update mail set name sender email

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -1,14 +1,22 @@
+import config from '../config'
 import mail from '../config/mailer'
 
 export interface Payload {
-  from: string;
-  to: string;
-  subject: string;
-  text: string;
-  html?: string;
+  to: string
+  subject: string
+  text: string
+  html?: string
 }
 
-export const sendMail = (mailOptions: Payload): void => {
+export const sendMail = (payload: Payload): void => {
+  const mailOptions = {
+    from: {
+      name: config.get('mail.from.name'),
+      address: config.get('mail.from.address')
+    },
+    ...payload
+  }
+
   mail.sendMail(mailOptions, (err) => {
     if (err) throw err
   })
