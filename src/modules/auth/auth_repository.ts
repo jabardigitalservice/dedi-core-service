e28f7c17e302d0cf2d0eedc1c8a9465f3178b752
ayocodingit <ayocodingit@gmail.com>
2021-11-04 18:47:51 +0700

    check company

--- src/modules/auth/auth_repository.ts
+++ src/modules/auth/auth_repository.ts
@@ -32,7 +32,7 @@ export namespace Auth {
       name: requestBody.company
     }
 
-    if (await isPartnerIdNotExist(requestBody)) {
+    if (requestBody.company && await isPartnerIdNotExist(requestBody)) {
       createPartner(Partner)
       return Partner.id
     }
