e7cc6462ab6408b8bd3bd9b237286532a23c1807
ayocodingit <ayocodingit@gmail.com>
2021-11-04 08:42:56 +0700

    rollback remove from test

--- src/modules/testimonials/testimonial_test.ts
+++ src/modules/testimonials/testimonial_test.ts
@@ -21,6 +21,7 @@ describe('seed data', () => {
 
 const expectMeta = expect.objectContaining({
   current_page: expect.any(Number),
+  from: expect.any(Number),
   last_page: expect.any(Number),
   per_page: expect.any(Number),
   to: expect.any(Number),
