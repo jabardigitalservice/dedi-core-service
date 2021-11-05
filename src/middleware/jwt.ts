1d8953d41aefc187ac391eb92b6aecac28138893
ayocodingit <ayocodingit@gmail.com>
2021-11-05 12:21:21 +0700

    jwt middleware: cookies access token

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -11,8 +11,8 @@ export default Jwt({
       return req.headers.authorization.split(' ')[1]
     } else if (req.query && req.query.token) {
       return req.query.token
-    } else if (req.cookies && req.cookies.token) {
-      return req.cookies.token
+    } else if (req.cookies && req.cookies.access_token) {
+      return req.cookies.access_token
     }
     return null
   }
