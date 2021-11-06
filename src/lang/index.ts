6aa3177b464f3cdeceaebfe318fc7d292eae9293
ayocodingit <ayocodingit@gmail.com>
2021-11-06 14:30:33 +0700

    add default language

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,9 +1,13 @@
 import lang from 'i18n'
 import path from 'path'
+import config from '../config'
 
 lang.configure({
-  locales: ['en'],
-  directory: path.join(__dirname, 'locales')
+  locales: ['en', 'id'],
+  directory: path.join(__dirname, 'locales'),
+  register: global
 })
+
+lang.setLocale(config.get('default.language', 'en'))
 
 export default lang
