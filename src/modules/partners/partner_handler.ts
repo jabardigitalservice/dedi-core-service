47a1bd4161799fbd2d049b9b0c52db6a9e00f37d
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-11-12 13:29:29 +0700

    type(partners): add type for result in handler

--- src/modules/partners/partner_handler.ts
+++ src/modules/partners/partner_handler.ts
@@ -21,7 +21,7 @@ router.get(
 router.get(
   '/v1/partnersUsingCursor',
   async (req: Request<never, never, never, Entity.RequestQueryUsingCursor>, res: Response, next: NextFunction) => {
-    const result: any = await Service.findAllUsingCursor(req.query)
+    const result: Entity.ResponseFindAllUsingCursor = await Service.findAllUsingCursor(req.query)
 
     res.status(httpStatus.OK).json(result)
   }
