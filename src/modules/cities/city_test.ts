91d0297c5c6bdf4c9547fb3e8ca59faa487269e3
ayocodingit <41193120+ayocodingit@users.noreply.github.com>
2021-11-25 15:54:37 +0700

    modify handler with location (#61)
    * modify handler with location
    
    * fix test cities
    
    * remove unused file

--- src/modules/cities/city_test.ts
+++ src/modules/cities/city_test.ts
@@ -8,7 +8,7 @@ describe('seed data', () => {
     await Repository.Cities().insert({
       id: '12345',
       name: 'test123',
-      location: database.raw('ST_GeomFromText(\'POINT(106.8207875 -6.4605558)\')'),
+      location: database.raw('ST_GeomFromText(\'POINT(107.5090974 -6.8342172)\')'),
       is_active: true,
     })
   })
@@ -26,47 +26,18 @@ const expectBodyFindAll = expect.arrayContaining([
 ])
 
 describe('tests cities', () => {
-  it('test success findAll with location ', async () => request(app)
-    .get('/v1/cities/list-with-location')
-    .expect(200)
-    .then((response) => {
-      expect(response.body).toEqual(expect.objectContaining({
-        data: expectBodyFindAll,
-        meta: expect.objectContaining({
-          total: expect.any(Number),
-        }),
-      }))
-    }))
-})
-
-describe('tests cities', () => {
-  it('test success findAll with location filter', async () => request(app)
-    .get('/v1/cities/list-with-location')
-    .query({ name: 'test123' })
-    .expect(200)
-    .then((response) => {
-      expect(response.body).toEqual(expect.objectContaining({
-        data: expectBodyFindAll,
-        meta: expect.objectContaining({
-          total: expect.any(Number),
-        }),
-      }))
-    }))
-})
-
-describe('tests cities', () => {
   it('test success findAll with location filter bounds', async () => request(app)
-    .get('/v1/cities/list-with-location')
+    .get('/v1/cities/with-location')
     .query({
       bounds: {
-        ne: '106.8207875, -6.4605558',
-        sw: '106.8207875, -6.4605558',
+        sw: '107.4312207548229,-7.044551821267334',
+        ne: '107.78594184930455,-6.79575221317816',
       },
     })
     .expect(200)
     .then((response) => {
       expect(response.body).toEqual(expect.objectContaining({
-        data: [],
+        data: expectBodyFindAll,
         meta: expect.objectContaining({
           total: expect.any(Number),
         }),
