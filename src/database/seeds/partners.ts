32bf579947edb08782cd33076967bc7c218252ec
hafizulf <38736580+hafizulf@users.noreply.github.com>
2021-11-29 14:32:07 +0700

    Sprint-3/partners seeder (#66)
    * add partners seeder
    
    * modify category_id to be null
    
    * remove updated_at & deleted_at
    
    * modify min-max total_village value
    
    * remove category_id

---
+++ src/database/seeds/partners.ts
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from 'uuid'
+import faker from 'faker'
+import moment from 'moment';
+import database from '../../config/database';
+
+export async function seed(): Promise<void> {
+  const data = []
+
+  for (let index = 0; index < 10; index++) {
+    data.push({
+      id: uuidv4(),
+      name: faker.name.findName(),
+      verified_at: moment().subtract({ seconds: (index + 1) }).toDate(),
+      logo: faker.image.avatar(),
+      total_village: faker.datatype.number({ min: 200, max: 2000 }),
+      website: faker.internet.url(),
+      created_at: moment().subtract({ seconds: (index + 1) }).toDate(),
+    });
+  }
+
+  return database('partners').insert(data)
+}
