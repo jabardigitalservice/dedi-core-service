f9185ad753b79f9c86a9409b08c0a8f6b4c17d87
ayocodingit <41193120+ayocodingit@users.noreply.github.com>
2021-12-08 13:43:01 +0700

    fix: remove rules find all (#69)

--- src/modules/partners/partner_rules.ts
+++ src/modules/partners/partner_rules.ts
@@ -1,9 +1,1 @@
-import Joi from 'joi'
-
-export namespace Partner {
-  export const findAll = Joi.object({
-    name: Joi.string().allow(null),
-    per_page: Joi.number().allow(null),
-    current_page: Joi.number().allow(null),
-  })
-}
+export namespace Partner {}
