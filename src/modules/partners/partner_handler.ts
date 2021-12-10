521e31c4d57031d8aff59abb8a830a98ada454b0
Aldi Naufal Fitrah <aldifitrahx@gmail.com>
2021-12-10 15:21:23 +0700

    style(partner): use multiline for long params

--- src/modules/partners/partner_handler.ts
+++ src/modules/partners/partner_handler.ts
@@ -4,15 +4,23 @@ import { Partner as Entity } from './partner_entity'
 import { Partner as Service } from './partner_service'
 
 export namespace Partner {
-  export const findAll = async (req: Request<never, never, never, Entity.RequestQuery>, res: Response, next: NextFunction) => {
-    const result: Entity.ResponseFindAll = await Service.findAll(req.query)
+  export const findAll = async (
+    req: Request<never, never, never, Entity.RequestQuery>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const result: Entity.ResponseFindAll = await Service.findAll(req.query);
 
-    res.status(httpStatus.OK).json(result)
-  }
+    res.status(httpStatus.OK).json(result);
+  };
 
-  export const suggestion = async (req: Request<never, never, never, Entity.RequestQuerySuggestion>, res: Response, next: NextFunction) => {
-    const result: Entity.ResponseSuggestion = await Service.search(req.query)
+  export const suggestion = async (
+    req: Request<never, never, never, Entity.RequestQuerySuggestion>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const result: Entity.ResponseSuggestion = await Service.search(req.query);
 
-    res.status(httpStatus.OK).json(result)
-  }
+    res.status(httpStatus.OK).json(result);
+  };
 }
