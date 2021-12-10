9a20d5c9ada21d7de250f2568acc6afce2bb80dc
hafizulf <hafizulf68@gmail.com>
2021-12-10 14:19:17 +0700

    modify handler

--- src/modules/testimonials/testimonial_handler.ts
+++ src/modules/testimonials/testimonial_handler.ts
@@ -1,21 +1,16 @@
-import express, { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import httpStatus from 'http-status'
-import cache from '../../config/cache'
 import { Testimonial as Entity } from './testimonial_entity'
 import { Testimonial as Service } from './testimonial_service'
-import { Testimonial as Log } from './testimonial_log'
 
-const router = express.Router()
-
-router.get(
-  '/v1/testimonials',
-  cache(),
-  Log.findAll(),
-  async (req: Request<never, never, never, Entity.RequestQuery>, res: Response, next: NextFunction) => {
+export namespace Testimonial {
+  export const testimonials = async (
+    req: Request<never, never, never, Entity.RequestQuery>,
+    res: Response,
+    next: NextFunction,
+  ) => {
     const result: Entity.ResponseFindAll = await Service.findAll(req.query)
 
     res.status(httpStatus.OK).json(result)
-  },
-)
-
-export default router
+  }
+}
