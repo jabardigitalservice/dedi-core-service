a8ddea68c6800e1de10dda03fc8f1eca052f8ad4
ayocodingit <ayocodingit@gmail.com>
2021-12-15 21:38:55 +0700

    helpers: role base access control pattern

--- src/helpers/roles.ts
+++ src/helpers/roles.ts
@@ -1,23 +1,4 @@
-import { AccessControl } from 'accesscontrol'
 import config from '../config';
-
-const accessControl = new AccessControl();
-
-accessControl.grant('basic')
-  .readOwn('profile')
-  .updateOwn('profile')
-
-accessControl.grant(config.get('role.0'))
-  .extend('basic')
-  .readAny('profile')
-
-accessControl.grant(config.get('role.1'))
-  .extend('basic')
-
-accessControl.grant(config.get('role.2'))
-  .extend('basic')
-
-export const roles = accessControl;
 
 export const getRole = (user: any) => {
   const partner = user.prtnr ? config.get('role.1') : config.get('role.2')
--- src/middleware/grantAccess.ts
+++ src/middleware/grantAccess.ts
@@ -1,10 +1,15 @@
+import { AccessControl } from 'accesscontrol'
 import { Request, Response, NextFunction } from 'express'
 import httpStatus from 'http-status'
 import { HttpError } from '../handler/exception'
-import { roles, getRole } from '../helpers/roles'
+import { getRole } from '../helpers/roles'
 import lang from '../lang'
 
-export const grantAccess = (action: string, resource: string) => async (req: Request, res: Response, next: NextFunction) => {
+export const grantAccess = (
+  roles: AccessControl,
+  action: string,
+  resource: string,
+) => async (req: Request, res: Response, next: NextFunction) => {
   const role = getRole(req.user)
   const permission = roles.can(role)[action](resource);
 
