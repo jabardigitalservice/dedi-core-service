b3540e4279bc93faa57a59d7af4e0a64438e6a88
ayocodingit <41193120+ayocodingit@users.noreply.github.com>
2021-12-16 09:30:40 +0700

    Helpers/rbac/pattern (#78)
    * helpers: role base access control pattern
    
    * helpers: refactor logic grant access
    
    * update readme

--- src/middleware/grantAccess.ts
+++ src/middleware/grantAccess.ts
@@ -1,14 +1,19 @@
+import { AccessControl } from 'accesscontrol'
 import { Request, Response, NextFunction } from 'express'
 import httpStatus from 'http-status'
 import { HttpError } from '../handler/exception'
-import { roles, getRole } from '../helpers/roles'
+import { getRole } from '../helpers/roles'
 import lang from '../lang'
 
-export const grantAccess = (action: string, resource: string) => async (req: Request, res: Response, next: NextFunction) => {
+export const grantAccess = (
+  roles: AccessControl,
+  action: string,
+  resource: string,
+) => async (req: Request, res: Response, next: NextFunction) => {
   const role = getRole(req.user)
   const permission = roles.can(role)[action](resource);
 
-  if (!permission.granted) next(new HttpError(httpStatus.UNAUTHORIZED, lang.__('auth.grant.access')));
+  if (permission.granted) return next();
 
-  next()
+  next(new HttpError(httpStatus.UNAUTHORIZED, lang.__('auth.grant.access')))
 }
