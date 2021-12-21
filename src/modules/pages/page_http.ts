12fee80b7ce486a7de0aa49c46732869c84eeb02
ayocodingit <ayocodingit@gmail.com>
2021-12-22 06:44:00 +0700

    add cache endpoint pages

--- src/modules/pages/page_http.ts
+++ src/modules/pages/page_http.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import cache from '../../config/cache';
 import { validate } from '../../helpers/validator';
 import { Page as Handler } from './page_handler';
 import { Page as Rules } from './page_rules';
 
 const router = Router()
 
-router.get('/v1/pages', validate(Rules.findAll, 'query'), Handler.findAll)
+router.get('/v1/pages', cache(), validate(Rules.findAll, 'query'), Handler.findAll)
 
 export default router
