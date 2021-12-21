5c7b3f082643fd7e2169deaca1d75bf9dcf07a45
ayocodingit <ayocodingit@gmail.com>
2021-12-21 12:18:56 +0700

    remove verify access token

--- src/modules/pages/page_http.ts
+++ src/modules/pages/page_http.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'
-import { verifyAccessToken } from '../../middleware/jwt';
 import { Page as Handler } from './page_handler';
-import { Page as Access } from './page_access';
 
 const router = Router()
 
-router.get('/v1/pages', verifyAccessToken, Access.findAll(), Handler.findAll)
+router.get('/v1/pages', Handler.findAll)
 
 export default router
