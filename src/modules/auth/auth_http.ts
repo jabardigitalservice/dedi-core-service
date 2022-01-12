e2229b0ef8143e09ea5cf21fd130f5724a0e0402
ayocodingit <ayocodingit@gmail.com>
2022-01-12 15:21:50 +0700

    feat: auth sign in limit response

--- src/modules/auth/auth_http.ts
+++ src/modules/auth/auth_http.ts
@@ -1,14 +1,23 @@
 import { Router } from 'express'
 import rateLimit from 'express-rate-limit'
+import httpStatus from 'http-status'
 import { validate, validateWithDB } from '../../helpers/validator'
 import { verifyAccessToken } from '../../middleware/jwt'
 import { Auth as Rules } from './auth_rules'
 import { Auth as Handler } from './auth_handler'
 import config from '../../config'
+import lang from '../../lang'
 
 const apiLimiterSignIn = rateLimit({
   windowMs: Number(config.get('api.limiter.time.signin', 300000)), // Default time signin 5 minutes
   max: Number(config.get('api.limiter.max.signin', 3)),
+  handler: (request, response, next) => {
+    const { resetTime } = request.rateLimit
+    response.status(httpStatus.TOO_MANY_REQUESTS).json({
+      error: lang.__('auth.signin.failed.limiter'),
+      reset_time: resetTime,
+    })
+  },
 });
 
 const router = Router()
