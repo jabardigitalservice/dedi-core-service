20113807dd4e3d67c9613823a497267ef8b74577
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2022-03-02 13:17:01 +0700

    Update gcs.ts

--- src/helpers/gcs.ts
+++ src/helpers/gcs.ts
@@ -24,6 +24,4 @@ export const uploadGCS = (file: Express.Multer.File): string => {
   return filename
 }
 
-export const getUrlGCS = (path: string) => {
-  return path ? `${config.get('gcs.cdn')}/${config.get('node.env')}/${path}` : null
-}
+export const getUrlGCS = (path: string) => path ? `${config.get('gcs.cdn')}/${config.get('node.env')}/${path}` : null
