78e3f3a099e3ef065b7758a51338ffcd3226d5d3
ayocodingit <ayocodingit@gmail.com>
2022-03-25 08:34:50 +0700

    modify: update rule password store (users)

--- src/modules/users/user_rules.ts
+++ src/modules/users/user_rules.ts
@@ -5,10 +5,6 @@ export namespace User {
   const orderByValid = ['name', 'is_active', 'email', 'updated_at']
   const emptyAllow = ['', null]
 
-  const regexPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9&*_]+)$/
-
-  const password = () => Joi.string().min(6).max(72).regex(regexPassword)
-
   export const findAll = Joi.object({
     order_by: Joi.string().valid(...orderByValid).allow(...emptyAllow),
     is_admin: Joi.boolean().allow(...emptyAllow),
@@ -16,7 +12,7 @@ export namespace User {
   })
 
   const rulesPassword = {
-    password: password().required(),
+    password: Joi.string().min(8).required(),
   }
 
   const email = Joi.string().email().max(150).required()
