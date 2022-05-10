957250de438b38f4d9e7446bfc4d3dc3dc63337f
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2022-05-10 10:27:47 +0700

    refactor: rule questionnaire (#145)
    * refactor: rule questionnaire
    
    * ecommerce lainnya

--- src/modules/villages/village_rules.ts
+++ src/modules/villages/village_rules.ts
@@ -10,93 +10,84 @@ export namespace Village {
 
   const ruleArrayString = Joi.array().items(Joi.string()).required().min(1)
 
-  const fasilitas_desa = Joi.alternatives().conditional('...level', {
-    is: Joi.number().valid(1, 2, 3, 4),
-    then: Joi.object({
-      akses_kendaraan: Joi.object({
-        data: ruleArrayString,
-        photo: file,
-      }).required(),
-      suplai_listrik: Joi.object({
-        data: Joi.string().required(),
-        photo: file,
-      }).required(),
-      jaringan_telepon: Joi.object({
-        data: Joi.string().required(),
-        photo: file,
-        operator: Joi.string().allow(null),
-      }).required(),
-      jaringan_internet: Joi.object({
-        data: Joi.string().required(),
-        photo: file,
-        website: Joi.string().allow(null),
-      }).required(),
-    }).required(),
-    otherwise: Joi.optional(),
-  })
-
-  const literasi_digital = Joi.alternatives().conditional('...level', {
-    is: Joi.number().valid(2, 3, 4),
-    then: Joi.object({
-      komunitas: Joi.object({
-        data: ruleArrayString,
-        photo: file,
-      }).required(),
-      pelatihan: Joi.object({
-        data: Joi.string().required(),
-        photo: file,
-        pelatihan: Joi.string().allow(null),
-      }).required(),
-    }).required(),
-    otherwise: Joi.optional(),
-  })
-
-  const tentang_bumdes = Joi.alternatives().conditional('...level', {
-    is: Joi.number().valid(3, 4),
-    then: Joi.object({
-      sosial_media: Joi.object({
-        data: ruleArrayString,
-        photo: file,
-      }).required(),
-      bumdes: Joi.object({
-        data: Joi.string().required(),
-        photo: file,
-        bumdes: Joi.string().allow(null),
-      }).required(),
-      komoditas: Joi.object({
-        data: Joi.string().allow(null),
-        photo: file,
-        produktivitas: Joi.string().required(),
-      }).required(),
-      ecommerce: Joi.object({
-        data: ruleArrayString,
-        ecommerce: Joi.string().allow(null),
-        distribusi: Joi.string().allow(null),
-      }),
-      logistik: Joi.string().required(),
-    }).required(),
-    otherwise: Joi.optional(),
-  })
-
-  const potensi_desa = Joi.alternatives().conditional('...level', {
-    is: Joi.number().valid(4),
-    then: Joi.object({
-      data: ruleArrayString.max(3),
+  const ruleLevel1 = Joi.object({
+    akses_kendaraan: Joi.object({
+      data: ruleArrayString,
       photo: file,
-      potensi_lainnya: Joi.string().allow(null),
-      potensi_dapat_dikembangkan: Joi.string().allow(null),
     }).required(),
-    otherwise: Joi.optional(),
-  })
+    suplai_listrik: Joi.object({
+      data: Joi.string().required(),
+      photo: file,
+    }).required(),
+    jaringan_telepon: Joi.object({
+      data: Joi.string().required(),
+      photo: file,
+      operator: Joi.string().allow(null),
+    }).required(),
+    jaringan_internet: Joi.object({
+      data: Joi.string().required(),
+      photo: file,
+      website: Joi.string().allow(null),
+    }).required(),
+  }).required()
+
+  const ruleLevel2 = Joi.object({
+    komunitas: Joi.object({
+      data: ruleArrayString,
+      photo: file,
+    }).required(),
+    pelatihan: Joi.object({
+      data: Joi.string().required(),
+      photo: file,
+      pelatihan: Joi.string().allow(null),
+    }).required(),
+  }).required()
+
+  const ruleLevel3 = Joi.object({
+    sosial_media: Joi.object({
+      data: ruleArrayString,
+      photo: file,
+    }).required(),
+    bumdes: Joi.object({
+      data: Joi.string().required(),
+      photo: file,
+      bumdes: Joi.string().allow(null),
+    }).required(),
+    komoditas: Joi.object({
+      data: Joi.string().allow(null),
+      photo: file,
+      produktivitas: Joi.string().required(),
+    }).required(),
+    ecommerce: Joi.object({
+      data: ruleArrayString,
+      ecommerce_lainnya: Joi.string().allow(null),
+      distribusi: Joi.string().allow(null),
+    }),
+    logistik: Joi.string().required(),
+  }).required()
+
+  const ruleLevel4 = Joi.object({
+    data: ruleArrayString.max(3),
+    photo: file,
+    potensi_lainnya: Joi.string().allow(null),
+    potensi_dapat_dikembangkan: Joi.string().allow(null),
+  }).required()
+
+  const getRuleLevel = (ruleLevelSchema: Joi.Schema, ruleLevelValid: number[]) => Joi.alternatives()
+    .conditional('...level', {
+      is: Joi.number().valid(...ruleLevelValid),
+      then: ruleLevelSchema,
+      otherwise: Joi.optional(),
+    })
 
   export const questionnaire = Joi.object({
     id: Joi.string().max(14).required(),
     level: Joi.number().valid(1, 2, 3, 4).required(),
     properties: Joi.object({
-      fasilitas_desa,
-      literasi_digital,
-      tentang_bumdes,
-      potensi_desa,
+      fasilitas_desa: getRuleLevel(ruleLevel1, [1, 2, 3, 4]),
+      literasi_digital: getRuleLevel(ruleLevel2, [2, 3, 4]),
+      tentang_bumdes: getRuleLevel(ruleLevel3, [3, 4]),
+      potensi_desa: getRuleLevel(ruleLevel4, [4]),
     }).required(),
   })
 
