12aaa407f7b97e8db7165dd4d60c91d3e01975c4
ayocodingit <ayocodingit@gmail.com>
2022-05-17 08:04:09 +0700

    remove unused method

--- src/helpers/cloudStorage.ts
+++ src/helpers/cloudStorage.ts
@@ -1,15 +1,12 @@
 import config from '../config'
+import { isCloudStorageS3 } from './constant'
 import { uploadGCS } from './gcs'
 import { uploadS3 } from './s3'
 
-const getFullUrl = (path: string, cdn: string) => (path ? `${cdn}/${config.get('node.env')}/${path}` : null)
-
-const defaultCloudStorage = config.get('cloud.storage', 's3')
-
 export const getUrl = (path: string) => {
-  const cdn = defaultCloudStorage === 's3' ? config.get('aws.s3.cloudfront') : config.get('gcs.cdn')
+  const cdn = isCloudStorageS3() ? config.get('aws.s3.cloudfront') : config.get('gcs.cdn')
 
-  return getFullUrl(path, cdn)
+  return path ? `${cdn}/${config.get('node.env')}/${path}` : null
 }
 
-export const uploadFile = (file: Express.Multer.File) => (defaultCloudStorage === 's3' ? uploadS3(file) : uploadGCS(file))
+export const uploadFile = (file: Express.Multer.File) => (isCloudStorageS3() ? uploadS3(file) : uploadGCS(file))
--- src/helpers/constant.ts
+++ src/helpers/constant.ts
@@ -4,3 +4,4 @@ export const isNodeEnvProduction = () => config.get('node.env') === 'production'
 export const isNodeEnvDevelopment = () => config.get('node.env') === 'development'
 export const isNodeEnvTest = () => config.get('node.env') === 'test'
 export const convertToBoolean = (boolean: any) => ['true', 1, true].includes(boolean)
+export const isCloudStorageS3 = () => config.get('cloud.storage', 's3') === 's3'
