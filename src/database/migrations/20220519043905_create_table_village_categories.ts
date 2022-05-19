0c17c3e91992f1e762fbec90f80c2e6480f25b3b
ayocodingit <ayocodingit@gmail.com>
2022-05-19 13:30:16 +0700

    fix category id integer

--- src/database/migrations/20220519043905_create_table_village_categories.ts
+++ src/database/migrations/20220519043905_create_table_village_categories.ts
@@ -9,7 +9,7 @@ export async function up(knex: Knex): Promise<void> {
 
   return knex.schema.createTable(tableName, (table) => {
     table.increments('id').unsigned()
-    table.string('category_id').index()
+    table.integer('category_id').index()
     table.string('village_id', 14).index()
   })
 }
