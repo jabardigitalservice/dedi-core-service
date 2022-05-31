3e99222bae011b99d4e6ae1d7e2c400a537d7291
ayocodingit <ayocodingit@gmail.com>
2022-05-31 16:25:22 +0700

    feat: add endpoint download file

--- src/modules/files/file_http.ts
+++ src/modules/files/file_http.ts
@@ -5,5 +5,6 @@ import { File as Handler } from './file_handler'
 const router = Router()
 
 router.post('/v1/files/upload', verifyPublic, Handler.upload)
+router.get('/v1/files/download/:filename', verifyPublic, Handler.download)
 
 export default router
