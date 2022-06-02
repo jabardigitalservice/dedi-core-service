636bf27b8fb315520e7681dde6bb8495a646ace8
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2022-06-02 14:09:15 +0700

    fix: fixing length access token and refresh token (#158)

--- src/database/migrations/20211105021056_create_table_oauth_tokens.ts
+++ src/database/migrations/20211105021056_create_table_oauth_tokens.ts
@@ -10,8 +10,8 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(tableName, (table) => {
     table.uuid('id').primary()
     table.uuid('user_id').index()
-    table.string('access_token', 767).index()
-    table.string('refresh_token', 767).index()
+    table.string('access_token', 1000)
+    table.string('refresh_token', 1000)
     table.integer('expired_in', 11)
     table.boolean('is_active').defaultTo(true)
     table.timestamp('created_at').notNullable()
