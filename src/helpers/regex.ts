0566e9b36f4c31a01e5a88dd40c545827b1a2db4
ayocodingit <ayocodingit@gmail.com>
2022-06-27 14:12:06 +0700

    fix: test

--- src/helpers/regex.ts
+++ src/helpers/regex.ts
@@ -1,6 +1,6 @@
 import config from '../config'
 
 export const regexExtFile = new RegExp(`([a-zA-Z0-9s_\\.-:])+(${config.get('file.type')})$`)
-export const regexAlphanumeric = /^[a-zA-Z0-9 .,-!@$_&*]+$/
+export const regexAlphanumeric = /^[a-zA-Z0-9 .,-_!@$&*?]+$/
 export const regexCodeRegion = /^[0-9.]+$/
-export const regexPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9&*+?.,^|&!@$_]+)$/
+export const regexPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9&*+?.,_^|&!@$]+)$/
