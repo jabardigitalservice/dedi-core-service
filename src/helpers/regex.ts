02510d1df9cdd3fa46b5a6953fe12c218c238ac3
ayocodingit <ayocodingit@gmail.com>
2022-07-07 21:53:04 +0700

    feat: update regex ext file case insensitive

--- src/helpers/regex.ts
+++ src/helpers/regex.ts
@@ -1,6 +1,6 @@
 import config from '../config'
 
-export const regexExtFile = new RegExp(`([a-zA-Z0-9s_\\.-:])+(${config.get('file.type')})$`)
+export const regexExtFile = new RegExp(`([a-zA-Z0-9s_\\.-:])+(${config.get('file.type')})$`, 'i')
 export const regexAlphanumeric = /^[a-zA-Z0-9\n .,_!@$&*?-]+$/
 export const regexCodeRegion = /^[0-9.]+$/
 export const regexPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9&*+?.,_^|&!@$]+)$/
