76288cb0cdfe93b3fba5369b2a2dd8505216f22f
ayocodingit <ayocodingit@gmail.com>
2022-08-24 11:42:43 +0700

    update regex point XX.XXXX or -XX.XXXX

--- src/helpers/regex.ts
+++ src/helpers/regex.ts
@@ -4,5 +4,5 @@ export const regexExtFile = new RegExp(`([a-zA-Z0-9s_\\.-:])+(${config.get('file
 export const regexAlphanumeric = /^[a-zA-Z0-9\n .,_!@$&*?-]+$/
 export const regexCodeRegion = /^[0-9.]+$/
 export const regexPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9&*+?.,_^|&!@$]+)$/
-export const regexPoint = /^(-?\d+(\.\d+)?)$/
+export const regexPoint = /^-?(\d+)(\.\d+)$/
 export const regexPointBounds = /^(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)$/
